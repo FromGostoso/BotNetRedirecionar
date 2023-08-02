401732a17a254199a2686fc1c4a1dfee26d1e225
FromGostoso <137526449+FromGostoso@users.noreply.github.com>
2023-08-02 17:22:10 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+    function bot() {
+      document.getElementById("bo").innerHTML = "Entrando...";
+      
+      setTimeout(() => {
+        window.location.href = "https://discord.gg/b5SNr6JSb2"
+     }, 2700)
+      setTimeout(() => {
+        window.close()
+     }, 4000)
+    }
